c7a1d0b05e959246aa760ce8b318f1056053d7ce
rumeysaturgut <rumeysa.turgut@hepsiburada.com>
2021-10-20 23:21:11 +0300

    initial commit

---
+++ PrimeNumberCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumberCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primeNumbers = new List<int>();
+            for (int i = 3; i < 10000000; i++)
+            {
+                if (IsGivenNumberPrime(i))
+                    primeNumbers.Add(i);
+            }
+            Console.WriteLine(String.Join(", ",  primeNumbers));
+        }
+        
+        private static bool IsGivenNumberPrime(int i)
+        {
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
